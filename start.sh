776069601e31872b15cb1bfb14012b5618a4c0ee
Costello90 <alextasssss@gmail.com>
2023-10-09 10:56:47 +0300

    remove call createsuperuser from start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,5 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 
-# create a superuser in Django admin
-python manage.py createsuperuser --no-input
-
 # start the Django server
 python manage.py runserver 0.0.0.0:8000
