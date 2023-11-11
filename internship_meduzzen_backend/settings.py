53887db94d1ad4db4c2fc503d009abc55707a98b
Costello90 <alextasssss@gmail.com>
2023-11-12 01:46:33 +0200

    change settings.py

--- internship_meduzzen_backend/settings.py
+++ internship_meduzzen_backend/settings.py
@@ -170,7 +170,9 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # Extra places for collect static files.
 
